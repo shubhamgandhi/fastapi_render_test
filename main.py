3885a37420b1eeda4456b1d508a86c050570f484
shubhamgandhi <shubham.gandhi@gmail.com>
2024-01-08 10:46:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def read_root():
     return {'Hello': 'World'}
 
 
-frontend.init(app)
+init(app)
 
 if __name__ == '__main__':
     print('Please start the app with the "uvicorn" command as shown in the start.sh script')
